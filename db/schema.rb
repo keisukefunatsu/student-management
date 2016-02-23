11b1896a9e69387a9402924419f0877fc757a278
pyon <you@example.com>
2016-02-23 13:22:43 +0900

    before heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160218102818) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "information", force: :cascade do |t|
     t.string   "title",       null: false
     t.text     "content",     null: false
@@ -51,7 +54,7 @@ ActiveRecord::Schema.define(version: 20160218102818) do
     t.boolean  "admin",                  default: false, null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
