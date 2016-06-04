01c8d2d032f51ca9617c15c3da3902b4da4d071f
chisaco <chisaco0403@gmail.com>
2016-06-04 14:51:53 +0900

    マイグレーション修正

--- db/migrate/20160604042640_add_author_to_results.rb
+++ db/migrate/20160604042640_add_author_to_results.rb
@@ -1,5 +1,6 @@
 class AddAuthorToResults < ActiveRecord::Migration
   def change
-      add_column :results, :author_id, :integer, default: 0, null: false
+      add_column :results, :author_id, :integer
+      add_index :results, :author_id
   end
 end
