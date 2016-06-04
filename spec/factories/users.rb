0fcc2187f0b1363875aac36b99c61987d5e646f9
Masuo KOBAYASHI <maskoba5010@gmail.com>
2016-06-04 07:47:34 +0000

    #55 お知らせ編集の[参加するか尋ねる]の状態を保存されている値で表示する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryGirl.define do
     password 'secret1234'
     password_confirmation { password }
   end
+  factory :admin_user, class: User  do
+    name { Faker::Name.name }
+    email 'admin@example.com'
+    password '00000000'
+    password_confirmation { password }
+    admin true
+    confirmed true
+  end
 end
